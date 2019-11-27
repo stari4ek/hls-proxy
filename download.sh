b14aa8eb2b473ac93c571dffc96487c57f948eb4
Fernando Ruiz Humanes <midrandir@gmail.com>
2019-11-27 12:41:30 +0100

    Fix url

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ ID=$1
 DIR=${OUTDIR:-"/var/hls"}
 MINUTES=${2:-10}
 TIMEOUT=$(echo  $MINUTES*60 | bc)
-pipenv run -- ./hlsproxy.py -d -o "$DIR/${ID::2}/$ID" "https://openwebinars.net/hls/$ID.m3u8" &
+pipenv run -- ./hlsproxy.py -d -o "$DIR/${ID::2}/$ID" "https://openwebinars.net/academia/hls/$ID.m3u8" &
 echo "Process $! will be killed after $TIMEOUT seconds"
 sleep $TIMEOUT && kill $!
 echo "Sended KILL to $!"
