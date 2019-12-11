7f22f3fae050f04467ee3066f2c308354cea84c2
Fernando Ruiz Humanes <midrandir@gmail.com>
2019-12-11 11:35:41 +0100

    Accept options as argumemnts

--- download.sh
+++ download.sh
@@ -4,8 +4,9 @@ set -e
 ID=$1
 DIR=${OUTDIR:-"/var/hls"}
 MINUTES=${2:-10}
+OPTIONS=${3:-'-d'}
 TIMEOUT=$(echo  $MINUTES*60 | bc)
-pipenv run -- ./hlsproxy.py -d -o "$DIR/${ID::2}/$ID" "https://openwebinars.net/academia/hls/$ID.m3u8" &
+pipenv run -- ./hlsproxy.py $OPTIONS -o "$DIR/${ID::2}/$ID" "https://openwebinars.net/academia/hls/$ID.m3u8" &
 echo "Process $! will be killed after $TIMEOUT seconds"
 sleep $TIMEOUT && kill -15 $!
 echo "Sended SIGTERM to $!"
